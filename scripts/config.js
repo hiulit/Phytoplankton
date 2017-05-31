23fa37263b367e59914b70ec8e16ffe9f4167d2f
Xavier Gómez <xavi@bestiario.org>
2017-05-31 09:49:40 +0200

    Created config file

---
+++ scripts/config.js
@@ -0,0 +1,23 @@
+var config = {
+  menu: [
+    {
+      title: 'Phytoplankton Style Guide',
+      url: [
+        'readme.css'
+      ]
+    },
+      {
+      title: 'Documentation',
+      url: [
+        'documentation.css'
+      ]
+    },
+      {
+      title: 'Plugins',
+      url: [
+        'fixie.css',
+        'prism.css'
+      ]
+    }
+  ]
+}
