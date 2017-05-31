a4a98382f496ef4885ae65eed5567a7a72631e19
Xavier Gómez <xavi@bestiario.org>
2017-05-31 15:36:50 +0200

    Added getFiles() function

---
+++ scripts/getFiles.js
@@ -0,0 +1,23 @@
+var getFiles = function(dirPath) {
+  var filesArray = filesArray || [];
+  $.ajax({
+    url: dirPath,
+    async: false,
+    cache: true,
+    success: function(data){
+      $(data).find("a").slice(1).each(function(index){
+        dataUrl = $(this).attr("href");
+
+        newDirPath = dirPath + dataUrl;
+
+        if (dataUrl.slice(-1) === '/') {
+          that.get_files(newDirPath);
+        } else {
+          filesArray.push(newDirPath);
+        }
+      });
+    }
+  });
+
+  return filesArray;
+};
